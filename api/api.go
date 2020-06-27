d2594310d7c0987db83f83e1db56da34f7770061
Edi Santoso <repodevs@gmail.com>
2020-06-27 12:52:21 +0700

    PART4: Refactor readBody and apiResponse

--- api/api.go
+++ api/api.go
@@ -31,44 +31,48 @@ type ErrResponse struct {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
+	body := readBody(r)
 
 	var formattedBody Login
-	err = json.Unmarshal(body, &formattedBody)
+	err := json.Unmarshal(body, &formattedBody)
 	helpers.HandleErr(err)
 	login := users.Login(formattedBody.Username, formattedBody.Password)
 
-	if login["message"] == "ok" {
-		resp := login
-		json.NewEncoder(w).Encode(resp)
-	} else {
-		resp := ErrResponse{Message: "Wrong username or password"}
-		json.NewEncoder(w).Encode(resp)
-	}
+	apiResponse(login, w)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
+	body := readBody(r)
 
 	var formattedBody Register
-	err = json.Unmarshal(body, &formattedBody)
+	err := json.Unmarshal(body, &formattedBody)
 	helpers.HandleErr(err)
 
 	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
 
-	if register["message"] == "ok" {
-		resp := register
+	apiResponse(register, w)
+}
+
+// readBody from http.Request
+func readBody(r *http.Request) []byte {
+	body, err := ioutil.ReadAll(r.Body)
+	helpers.HandleErr(err)
+	return body
+}
+
+// apiResponse used for response
+func apiResponse(call map[string]interface{}, w http.ResponseWriter) {
+	if call["message"] == "ok" {
+		resp := call
 		json.NewEncoder(w).Encode(resp)
 	} else {
-		// resp := ErrResponse{Message: "Invalid Registration Data"}
-		resp := register
+		resp := ErrResponse{Message: "invalid data"}
+		// resp := call
 		json.NewEncoder(w).Encode(resp)
 	}
 }
 
-// StartAPI used for string API using mux
+// StartAPI used for starting API using mux
 func StartAPI() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", login).Methods("POST")
