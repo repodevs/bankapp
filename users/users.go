3daec3755e7416f3d2c1e855fcaeed37bd40aaa9
Edi Santoso <repodevs@gmail.com>
2020-06-27 11:33:40 +0700

    PART3: Refactor Login

--- users/users.go
+++ users/users.go
@@ -29,25 +29,35 @@ func Login(username string, pass string) map[string]interface{} {
 	accounts := []interfaces.ResponseAccount{}
 	db.Table("accounts").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&accounts)
 
-	responseUser := &interfaces.ResponseUser{
-		ID: user.ID,
-		Username: user.Username,
-		Email: user.Email,
-		Accounts: accounts,
-	}
+	var response = prepareResponse(user, accounts)
 
+	return response
+}
+
+func prepareToken(user *interfaces.User) string {
 	tokenContent := jwt.MapClaims{
 		"user_id": user.ID,
-		"expiry": time.Now().Add(time.Minute * 60).Unix(), 
+		"expiry":  time.Now().Add(time.Minute * 60).Unix(),
 	}
 	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
 	token, err := jwtToken.SignedString([]byte("TokenPassword"))
 	helpers.HandleErr(err)
 
+	return token
+}
+
+func prepareResponse(user *interfaces.User, accounts []interfaces.ResponseAccount) map[string]interface{} {
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
+	var token = prepareToken(user)
 	var response = map[string]interface{}{"message": "ok"}
 	response["jwt"] = token
 	response["data"] = responseUser
 
 	return response
 }
-
